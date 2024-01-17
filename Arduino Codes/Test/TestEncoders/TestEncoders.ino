7f4d0becc2c695cfc6a5eef04a1cb4db15319c71
Panagiotis Liampas <pliam1105@gmail.com>
2024-01-17 21:18:16 +0200

    Add motor & encoder testing codes

---
+++ Arduino Codes/Test/TestEncoders/TestEncoders.ino
@@ -0,0 +1,62 @@
+//left encoder
+#define leftA 2
+#define leftB 3
+
+//right encoder
+#define rightA 19
+#define rightB 18
+
+volatile long leftCounter = 0, rightCounter = 0;
+const long interval = 1000;
+unsigned long lastTime, currentTime;
+bool changeLeft = false, changeRight = false;
+
+void setup() {
+  Serial.begin(115200);
+  //set pin modes
+  pinMode(leftA, INPUT_PULLUP);
+  pinMode(leftB, INPUT_PULLUP);
+  pinMode(rightA, INPUT_PULLUP);
+  pinMode(rightB, INPUT_PULLUP);
+
+  attachInterrupt(0, changeLeftA, CHANGE);
+  attachInterrupt(4, changeRightA, CHANGE);
+
+  lastTime = millis();
+}
+
+void loop() {
+  currentTime = millis();
+  if(currentTime - lastTime >= interval){
+    //compute speeds in pulses/second
+    float leftSpeed = ((float)leftCounter)/((float)currentTime - lastTime);
+    float rightSpeed = ((float)rightCounter)/((float)currentTime - lastTime);
+    Serial.print(leftSpeed);
+    Serial.print(", ");
+    Serial.println(rightSpeed);
+    leftCounter = rightCounter = 0;
+    lastTime += interval;
+  }
+}
+
+void changeLeftA(){
+  if(digitalRead(leftA)!=digitalRead(leftB)){
+    //clockwise
+    leftCounter++;
+  }else{
+    //counterclockwise
+    leftCounter--;
+  }
+  changeLeft = true;
+}
+
+void changeRightA(){
+  if(digitalRead(rightA)!=digitalRead(rightB)){
+    //clockwise
+    rightCounter++;
+  }else{
+    //counterclockwise
+    rightCounter--;
+  }
+  changeRight = true;
+}
--- Arduino Codes/Test/TestMotors/TestMotors.ino
+++ Arduino Codes/Test/TestMotors/TestMotors.ino
@@ -1,9 +1,62 @@
-void setup() {
-  // put your setup code here, to run once:
+//left motor
+#define leftRPWM 4
+#define leftLPWM 5
+#define leftENR 8
+#define leftENL 9
 
+//right motor
+#define rightRPWM 6
+#define rightLPWM 7
+#define rightENR 10
+#define rightENL 11
+
+unsigned long startTime;
+
+void setup() {
+  //define motor pin modes
+  pinMode(leftRPWM, OUTPUT);
+  pinMode(leftLPWM, OUTPUT);
+  pinMode(rightRPWM, OUTPUT);
+  pinMode(rightLPWM, OUTPUT);
+  pinMode(leftENR, OUTPUT);
+  pinMode(leftENL, OUTPUT);
+  pinMode(rightENR, OUTPUT);
+  pinMode(rightENL, OUTPUT);
+
+  //enable motor movement in both directions
+  digitalWrite(leftENL, HIGH);
+  digitalWrite(leftENR, HIGH);
+  digitalWrite(rightENL, HIGH);
+  digitalWrite(rightENR, HIGH);
+
+  //forward
+  analogWrite(leftRPWM, 63);
+  analogWrite(leftLPWM, 0);
+  analogWrite(rightRPWM, 63);
+  analogWrite(rightLPWM, 0);
+
+  startTime = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //put both motors in straight rotation for 2 seconds
+  unsigned long endTime = millis();
+  if(endTime - startTime > 4000){
+    analogWrite(leftRPWM, 0);
+    analogWrite(leftLPWM, 0);
+    analogWrite(rightRPWM, 0);
+    analogWrite(rightLPWM, 0);
+    //disable movement
+    digitalWrite(leftENL, LOW);
+    digitalWrite(leftENR, LOW);
+    digitalWrite(rightENL, LOW);
+    digitalWrite(rightENR, LOW);
+  }else if(endTime - startTime > 2000){
+    //reverse
+    analogWrite(leftRPWM, 0);
+    analogWrite(leftLPWM, 63);
+    analogWrite(rightRPWM, 0);
+    analogWrite(rightLPWM, 63);
+  }
+  delay(100);
 }
